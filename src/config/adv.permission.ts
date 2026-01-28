dbda4c1d19988e4aa732e5f830a7e8673b94e4e1
Roshni Singh <roshni.singh@codeclouds.co.in>
2026-01-28 14:00:47 +0530

    files has been updated

--- src/config/adv.permission.ts
+++ src/config/adv.permission.ts
@@ -24,3 +24,29 @@ export const ACTIONS = {
     EDIT: 'edit',
     DELETE: 'delete'
 } as const;
+
+
+export type ModuleKey = keyof typeof MODULES;
+export type ActionType = typeof ACTIONS[keyof typeof ACTIONS];
+
+
+export const getPermission = (
+    moduleKey: ModuleKey,
+    action: ActionType,
+    submodule?: string
+): string => {
+    const module = MODULES[moduleKey].key;
+
+    return submodule
+        ? `${module}:${submodule}:${action}`
+        : `${module}:${action}`;
+};
+export const generateMatrix = (): string[] => {
+    const permissions: string[] = [];
+    Object.values(MODULES).forEach(module => {
+        Object.values(ACTIONS).forEach(action => {
+            permissions.push(`${module.key}:${action}`)
+        })
+    });
+    return permissions;
+}
