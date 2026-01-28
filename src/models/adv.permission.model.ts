930e6fa8df6d406e946d283a37cfdf8c8d8171c0
Roshni Singh <roshni.singh@codeclouds.co.in>
2026-01-28 15:36:20 +0530

    Model file has been added

---
+++ src/models/adv.permission.model.ts
@@ -0,0 +1,17 @@
+import mongoose, { Schema, Document } from 'mongoose';
+export interface IPermission extends Document {
+    name: String;
+    permissions: string[];
+    created_at: Date;
+    updated_at: Date;
+}
+
+const PermissionSchema: Schema = new Schema({
+    name: { type: String, required: true, unique: true },
+    permissions: { type: [String], required: true },
+}, {
+    timestamps: { createdAt: 'created_at', updatedAt: 'updated_at' },
+    versionKey: false,
+});
+
+export const Permission = mongoose.model<IPermission>('Permission', PermissionSchema);
