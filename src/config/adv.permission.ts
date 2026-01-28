1c8ee51c86d2b678316047cbdf87346f9b14b696
Roshni Singh <roshni.singh@codeclouds.co.in>
2026-01-28 13:51:46 +0530

    modules and actions has been set

---
+++ src/config/adv.permission.ts
@@ -0,0 +1,26 @@
+export const MODULES = {
+    PROFILE: {
+        key: 'profile',
+        submodules: {}
+    },
+    SETTINGS: {
+        key: 'settings',
+        submodules: {}
+    },
+    ACTIVITY: {
+        key: 'activity',
+        submodules: {}
+    },
+    SECURITY: {
+        key: 'security',
+        submodules: {}
+    }
+} as const;
+
+
+export const ACTIONS = {
+    VIEW: 'view',
+    CREATE: 'create',
+    EDIT: 'edit',
+    DELETE: 'delete'
+} as const;
