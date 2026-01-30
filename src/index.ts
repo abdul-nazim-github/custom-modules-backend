e132fa26ada7163ec889476ae2acc183a6d7a35c
Roshni Singh <roshni.singh@codeclouds.co.in>
2026-01-30 13:05:23 +0530

    Made the branch clear and clean

--- src/index.ts
+++ src/index.ts
@@ -3,9 +3,7 @@ import { AuthConfig } from './config/types.js';
 import { createAuthRoutes } from './routes/auth.routes.js';
 import { AuthController } from './controllers/auth.controller.js';
 import { AuthService } from './services/auth.service.js';
-import { SessionService } from './services/session.service.js';
 import { UserRepository } from './repositories/user.repository.js';
-import { SessionRepository } from './repositories/session.repository.js';
 
 export class AuthModule {
     private config: AuthConfig;
@@ -18,11 +16,9 @@ export class AuthModule {
     }
     private initialize() {
         const userRepository = new UserRepository();
-        const sessionRepository = new SessionRepository();
-        const sessionService = new SessionService(sessionRepository, this.config);
-        const authService = new AuthService(this.config, userRepository, sessionService);
+        const authService = new AuthService(this.config, userRepository);
         const authController = new AuthController(authService);
-        this.router.use('/auth', createAuthRoutes(authController, this.config.jwt.accessSecret, sessionRepository, userRepository));
+        this.router.use('/auth', createAuthRoutes(authController));
     }
 
     public static init(config: AuthConfig): AuthModule {
