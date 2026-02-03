be7a57bcf92dc11773095ca9891247b0d93d2665
Roshni Singh <roshni.singh@codeclouds.co.in>
2026-02-03 17:59:48 +0530

    Spaced has been fixed

--- src/middlewares/permission.middleware.ts
+++ src/middlewares/permission.middleware.ts
@@ -24,7 +24,6 @@ export const permissionMiddleware = (requiredPermission: string) => {
             }
             return false;
         });
-
         if (!hasPermission) {
             return res.status(403).json({
                 message: `Forbidden: You do not have the required permission (${requiredPermission})`,
